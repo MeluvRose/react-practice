bd46d76681a271e88b5d490d0a9f8a74907eaffd
meluvrose <tanggu3910@gmail.com>
2023-02-10 14:25:48 +0900

    #6.4 Cleanup Fuction

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
+  function ByFn() {
+    console.log("Destroyed :(");
+  }
+  function HiFn() {
+    console.log("Created :)");
+    return ByFn;
+  }
+  useEffect(HiFn, []);
+  return <h1>Hello</h1>;
+}
 
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("CALL THE API...");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' changes");
-  }, [counter]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div className="App">
-      <h1 onClick={onClick}>Welcome back to React</h1>
-      <h3>{counter}</h3>
-      <input
-        type="text"
-        placeholder="Search here..."
-        value={keyword}
-        onChange={onChange}
-      ></input>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
